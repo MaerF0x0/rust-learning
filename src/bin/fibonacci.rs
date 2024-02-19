ac1d92a36d4768535c534244dc3ef8925bc98cdb
Michael Graf <mikegraf000@gmail.com>
2024-02-19 13:09:55 -0600

    add memoization to fib using macros

--- src/bin/fibonacci.rs
+++ src/bin/fibonacci.rs
@@ -1,14 +1,44 @@
+use std::collections::HashMap;
 
-
-fn main() {
-    let n = 20;
-    println!("fib(n) = {}", fib(n))
+macro_rules! fib{
+    ($x: expr) => {fib_nohash($x)}; //call function f1 when there's one variable
+    ($x: expr, $y: expr) => {fib_hash($x, $y)}; //call f2 when there are two
 }
 
-fn fib(n: u32) -> u32 {
+fn main() {
+    let mut fibs = HashMap::<u32, u32>::new();
+
+    let n = 20;
+    println!("fib({}) = {}", n, fib!(n));
+    println!("fib({}) = {}", n, fib!(n, &mut fibs))
+}
+
+
+
+// Compute without a hash
+fn fib_nohash(n: u32) -> u32 {
     if n <= 2 {
-        return n
+        return n;
     }
 
-    return fib(n-1) + fib(n-2)
+
+    return fib_nohash(n-1) + fib_nohash(n-2);
+}
+
+// Compute using a hash to memoize
+fn fib_hash(n: u32, fibs: &mut HashMap<u32, u32>) -> u32 {
+    match fibs.get(&n) {
+        Some(n) => return *n,
+        None => {
+            if n <= 2 {
+                let this_fib = fib_nohash(n);
+                fibs.insert(n, this_fib);
+                return this_fib
+            }
+
+            let this_fib = fib_hash(n-1, fibs) + fib_hash(n-2, fibs);
+            fibs.insert(n, this_fib);
+            return this_fib
+        }
+    }
 }
