3545f511cc39e561c2ed231e62d02c8a5fae8d54
Michael Graf <mikegraf000@gmail.com>
2024-02-20 10:48:03 -0600

    transpose test

--- src/bin/transpose_matrix.rs
+++ src/bin/transpose_matrix.rs
@@ -11,6 +11,24 @@ fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
     return new_matrix;
 }
 
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+}
+
 fn main() {
     let matrix = [
         [101, 102, 103], // <-- the comment makes rustfmt add a newline
